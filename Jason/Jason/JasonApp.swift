5701a7db5fa0b1330e9378361f33c1bdf18b94d1
Timothy Velberg <timothy.velberg@gmail.com>
2025-07-31 15:25:42 +0200

    Initial commit

---
+++ Jason/Jason/JasonApp.swift
@@ -0,0 +1,17 @@
+//
+//  JasonApp.swift
+//  Jason
+//
+//  Created by Timothy Velberg on 31/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct JasonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
